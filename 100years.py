bddae4bbe5f3bec0ce08cb8d5614fc83c2791226
Keira Stewart <53539678+S-Cypher@users.noreply.github.com>
2021-09-04 20:06:00 -0500

    Add files via upload

---
+++ 100years.py
@@ -0,0 +1,9 @@
+#100 years old
+#Keira Stewart
+c_year = 2021
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+birth = c_year - age
+old = birth + 100
+
+print(name, ", you will be 100 years old in", old)
